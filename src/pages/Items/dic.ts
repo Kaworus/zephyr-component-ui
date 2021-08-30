a0f6cbac11238c3bed29d2e2f7ae8cff22a022b8
Kaworus <kaworu2016@163.com>
2021-08-30 17:21:10 +0800

    修改字典，修改获取数据方式

---
+++ src/pages/Items/dic.ts
@@ -0,0 +1,36 @@
+const rarityContent = {
+  Basic: '普通',
+  Masterwork: '精制',
+  Exotic: '稀有',
+  Rare: '特异',
+  Ascended: '升华',
+  Legendary: '传奇'
+}
+
+const rarityColor = {
+  Basic: 'rgba(156,156,156)',
+  Masterwork: '#1a9306',
+  Exotic: '#ffa405',
+  Rare: '#fcd00b',
+  Ascended: '#fb3e8d',
+  Legendary: '#4C139D'
+}
+
+const type = {
+  Container: '消耗品',
+  Consumable: '消耗品',
+  Gizmo: '小发明',
+  Bag: '包包',
+  Armor: '护甲',
+  MiniPet: '迷你宠物',
+  Trinket: '首饰',
+  Trophy: '战利品',
+  UpgradeComponent: '升级组件',
+  Back: '背部',
+  Gathering: '采集工具',
+  Trait: '特殊物品',
+  Tool: '工具',
+  CraftingMaterial: '制作材料'
+}
+
+export {rarityContent, type, rarityColor }
